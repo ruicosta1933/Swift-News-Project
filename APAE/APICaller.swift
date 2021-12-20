132f140610761e50e85fb52380f8fe384ef22b9e
ruicosta1933 <ruicosta607@gmail.com>
2021-12-20 21:55:59 +0000

    Getting News from API

---
+++ APAE/APICaller.swift
@@ -0,0 +1,8 @@
+//
+//  APICaller.swift
+//  APAE
+//
+//  Created by Rui Costa on 20/12/2021.
+//
+
+import Foundation
---
+++ APAE/NewsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsTableViewCell.swift
+//  APAE
+//
+//  Created by Rui Costa on 20/12/2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
