8dd30163ff4d85b9de4c136afd7e10baa4b85811
ruicosta1933 <ruicosta607@gmail.com>
2021-12-20 21:57:12 +0000

    Bugs Fixed

--- APAE/APICaller.swift
+++ APAE/APICaller.swift
@@ -1,8 +1,55 @@
-//
-//  APICaller.swift
-//  APAE
-//
-//  Created by Rui Costa on 20/12/2021.
-//
-
 import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+
+    struct Constants {
+        static let toHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=pt&apiKey=1d7225c5d26242dca298f42bbea8e1b8")
+    }
+    
+    private init() {}
+    
+    public func getTopNews(completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = Constants.toHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+// Modules
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String?
+}
+
+struct Source: Codable {
+    let name: String
+}
